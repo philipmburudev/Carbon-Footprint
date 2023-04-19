4afe5cbc039264fccf684e45058f7382b7a5301a
Aaron Obodai <aaron.torgbor@ashesi.edu.gh>
2023-04-19 16:35:53 +0000

    aaron

--- ModesOfTransport.java
+++ ModesOfTransport.java
@@ -1,5 +1,6 @@
 public class ModesOfTransport{
     public static void main(String[] args) {
         System.out.println("We are team carbon footprint");
+        System.out.println("We are team carbon footprint");
     }
 }
