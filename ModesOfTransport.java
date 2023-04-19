8dd683197cd235cef52bf5bfe5725d64d63557cc
Freda Beecham <freda.beecham@ashesi.edu.gh>
2023-04-19 16:48:02 +0000

    added new line

--- ModesOfTransport.java
+++ ModesOfTransport.java
@@ -2,5 +2,7 @@ public class ModesOfTransport{
     public static void main(String[] args) {
         System.out.println("We are team carbon footprint");
         System.out.println("We are team carbon footprint");
+        System.out.println("We are team carbon foooooooooooooootprint");
+    
     }
 }
