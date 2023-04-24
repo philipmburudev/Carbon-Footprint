1d04346ab2f048b554791f216c69127302eb2100
Freda Beecham <freda.beecham@ashesi.edu.gh>
2023-04-24 19:58:34 +0000

    freda-marie

--- FootprintTracker.java
+++ FootprintTracker.java
@@ -1,8 +1,15 @@
 import java.util.Scanner;
-
+import java.io.FileWriter;  
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.File;
+import java.lang.StringBuilder;
 import javax.lang.model.util.Elements.Origin;
 
 public class FootprintTracker {
+
+    static final int MAXIMUMUM_NUMBER_OF_LOCATIONS=20;
 
     public FootprintTracker(){}
 
@@ -12,6 +19,37 @@ public class FootprintTracker {
  
     //main method
     public static void main(String args[]){
+        try
+    {
+        Scanner inputStream = new Scanner (new File("C:\\Users\\User\\Desktop\\Carbon-Footprint\\Ghana.csv"));         
+        
+        String line = inputStream.nextLine();
+        String[] locationName= new String[MAXIMUMUM_NUMBER_OF_LOCATIONS];
+        double[] locationLongitude= new double[MAXIMUMUM_NUMBER_OF_LOCATIONS];
+        double[] locationLatitude= new double[MAXIMUMUM_NUMBER_OF_LOCATIONS];
+
+        int numberOfLocationss=0;
+        while (inputStream.hasNextLine())
+        {
+            line = inputStream.nextLine();
+            String[] locationsStringType = line.split(",");
+            locationName[numberOfLocationss]=locationsStringType[0];
+            locationLongitude[numberOfLocationss]=Double.parseDouble(locationsStringType[1]) ;
+            locationLatitude[numberOfLocationss]=Double.parseDouble(locationsStringType[2]) ;
+            numberOfLocationss++;                
+        }
+
+        for (int i=0;i<MAXIMUMUM_NUMBER_OF_LOCATIONS;i++)
+        {
+            System.out.println(locationLatitude[i]);
+        }
+        
+    }
+    catch (FileNotFoundException e)
+    {
+        System.out.println("Cannot find file");
+    }}
+    
         Scanner userInput= new Scanner(System.in);
         System.out.println("\n" + "Hello! This is Footprint Forward, here to help you implement sustainable practices in your daily life.");
         System.out.println("To start, you will have to input your origin, destination, and mode of transport you intend to use.\n");
